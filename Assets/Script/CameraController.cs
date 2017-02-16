d9d7c672598458b5b1bc2d1bcfda215b2c0930b2
Chaoyifu <chaoyifu@asu.edu>
2017-02-15 22:37:43 -0700

    Initial

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - player.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = player.transform.position + offset;
+        transform.position = new Vector3(player.transform.position.x + offset.x, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Script/rotateColider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateColider : MonoBehaviour {
+
+    void Start() {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("111");
+        Quaternion rot = other.transform.rotation;
+        rot.x = -180f;
+        other.transform.rotation = rot;
+        other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y - 3, other.transform.position.z);
+    }
+}
